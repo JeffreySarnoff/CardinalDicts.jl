c6a9291d91c1d68bd808070f15e2f5704eb4f1b3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 11:51:24 -0400

    Update NBitSets.jl

--- src/types/NBitSets.jl
+++ src/types/NBitSets.jl
@@ -3,35 +3,50 @@ module NBitSets
 export NBitSet,
        unsafe_getidx, unsafe_setidx!
 
-struct NBitSet{N}
-    value::Vector{Int16}
+struct NBitSet{T}
+    value::Vector{T}
     
-    function NBitSet(elements::E) where E<:Integer
-        nInt16s = cld(elements, 16)
-        zeroed  = zeros(Int16, nInt16s)
-        return new{elements}(zeroed)
+    function NBitSet(nbits::I) where I<:Integer
+        nbits > typemax(I)>>2 && throw(ErrorException("An NBitSet{$(I)} is limited to $(typemax(I)>>2) bits."))
+        noftype = cld(nbits, sizeof(I)%I)
+        zeroed  = zeros(I, noftype)
+        return new{I}(zeroed)
     end
 end
 
 Base.length(bitset::NBitSet{N}) where N = N%Int16
 
+const One8  = one(Int8)
 const One16 = one(Int16)
+const One32 = one(Int32)
+const One64 = one(Int64)
 
-@inline bitdex(index::Int16)::Int16 = fldmod(index, 16%Int16)
-@inline setbit(bits::Int16, index::Int16)::Int16 = bits | (One16 << (index-One16))
-@inline getbit(bits::Int16, index::Int16)::Int16 = (bits & (One16 << (index-One16))) !== zero(Int16)
+@inline function bitdex(index::I) where I<:Integer
+    offset, index = fldmod(index, sizeof(I)%I)
+    # offset = offset + one(I)
+    return offset, index
+end
+#@inline bitdex(index::I)::I where I<:Integer = fldmod(index, sizeof(I)%I)
+
+@inline setbit(bits::I, index::I) where I<:Integer =  (bits | (one(I) << (index-one(I))))::I
+@inline getbit(bits::I, index::I) where I<:Integer = ((bits | (one(I) << (index-one(I)))) !== zero(i))::Bool
+
+
+#@inline setbit(bits::I, index::I)::I where I<:Integer = bits | (one(I) << (index-one(I)))
+#@inline getbit(bits::I, index::I)::I = (bits & (One16 << (index-One16))) !== zero(Int16)
 
 function Base.getindex(bitset::NBitSet{N}, index::I) where I where N
     0 < index <= N || throw(ErrorException("index $(index) is outside of the defined domain (1:$(N))")) 
-    offset, bitidx = bitdex(index%Int16)
-    return One16 === getbit(bitset.value[offset+One16], bitidx)
+    offset, bitidx = bitdex(index)
+    return one(I) === getbit(bitset.value[offset+one(I)], bitidx)
  end
 
 function Base.setindex!(bitset::NBitSet{N}, value::Bool, index::I) where I where N
     0 < index <= N || throw(ErrorException("index $(index) is outside of the defined domain (1:$(N))")) 
-    offset, bitidx = bitdex(index%Int16)
+    offset, bitidx = bitdex(index)
     if value
-        bitset.value[offset+One16] = setbit(bitset.value[offset+One16], bitidx)
+        offset_plus1 = offset+one(I)
+        bitset.value[offset_plus1] = setbit(bitset.value[offset_plus1], bitidx)
     end
     return bitset
 end
@@ -39,13 +54,14 @@ end
 # module internal shortcuts
 
 function unsafe_getidx(bitset::NBitSet{N}, index::I) where I where N
-    offset, bitidx = bitdex(index%Int16)
-    return One16 === getbit(bitset.value[offset+One16], bitidx)
+    offset, bitidx = bitdex(index)
+    return one(I) === getbit(bitset.value[offset+one(I)], bitidx)
  end
 
 function unsafe_setidx!(bitset::NBitSet{N}, index::I) where I where N
-    offset, bitidx = bitdex(index%Int16)
-    bitset.value[offset+One16] = setbit(bitset.value[offset+One16], bitidx)
+    offset, bitidx = bitdex(index)
+    offset_plus1 = offset+one(I)
+    bitset.value[offset_plus1] = setbit(bitset.value[offset_plus1], bitidx)
     return bitset
 end
 
