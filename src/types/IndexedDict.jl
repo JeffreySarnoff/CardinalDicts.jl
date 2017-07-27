9e2ae4665975f7ca6377914a8b04c0098148d4a3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 14:08:06 -0400

    Update IndexedDict.jl

--- src/types/IndexedDict.jl
+++ src/types/IndexedDict.jl
@@ -1,31 +1,29 @@
 include("NBitSets.jl")
 using .NBitSets
 
-struct IndexedDict{N, K, V} <: Associative{K,V}
-    valued::NBitSet{N}
+struct IndexedDict{K,V} <: Associative{K,V}
+    valued::NBitSet{K}
     values::Vector{V}
     
-    function IndexedDict{V1}(n::N1) where N1<:Integer where V1
-        n16 = n%Int16
-        valued = NBitSet{n16}
-        values = Vector{V1}(n16)
-        return new{Int16,eltype(valued),V1}(valued, values)
+    function IndexedDict{V}(n::K) where K<:Integer where V
+        valued = NBitSet(n)
+        values = Vector{V}(n)
+        return new{K,V}(valued, values)
     end
 end
 
-@inline function Base.haskey(dict::IndexedDict{N,K,V}, key::K) where V where K<:Integer where N
+@inline function Base.haskey(dict::IndexedDict{K,V}, key::K) where K<:Integer where V
    getindex(dict.valued, key%Int16)
 end
 
-function Base.getindex(dict::IndexedDict{N,K,V}, key::K) where V where K<:Integer where N
-    cle = key%Int16
-    haskey(dict, cle) && return dict[cle]
+function Base.getindex(dict::IndexedDict{K,V}, key::K) where K<:Integer where V
+    haskey(dict, key) && return dict[key]
     throw(ErrorException("Key (index) $(key) has not been given a value"))
 end
 
-function Base.setindex!(dict::IndexedDict{N,K,V}, value::V, key::K) where V where K<:Integer where N
+function Base.setindex!(dict::IndexedDict{K,V}, value::V, key::K) where K<:Integer where V
     0 < key <= N || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(N)."))
-    dict.values[key%Int16] = value
+    dict.values[key] = value
     return dict
 end
 
