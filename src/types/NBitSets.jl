f9f8cc9d4940e7d42af6d43debd7deb13d077b78
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 03:34:03 -0400

    Update NBitSets.jl

--- src/types/NBitSets.jl
+++ src/types/NBitSets.jl
@@ -12,8 +12,17 @@ end
 Base.length(bitset::NBitSet{N}) where N<:Integer = N
 Base.typemax(bitset::NBitSet{N}) where N<:Integer = fldmod(N, 16)
 
-function hasvalue(bitset::NBitSet, index::N) where N<:Integer
+function Base.getindex(bitset::NBitSet{N}, index::I) where I<:Integer where N<:Integer
     0 < index <= N || throw(ErrorException("index $(index) is outside of the defined domain (1:$(N))")) 
     offset, bitidx = fldmod(index, 16)
     return one(Int16) === (bitset[offset+1] >> 15-bitidx) & one(Int16)
+end
+
+function Base.setindex!(bitset::NBitSet{N}, value::Bool, index::I) where I<:Integer where N<:Integer
+    0 < index <= N || throw(ErrorException("index $(index) is outside of the defined domain (1:$(N))")) 
+    offset, bitidx = fldmod(index, 16)
+    if value
+        bitset[offset+1] = bitset[offset+1] & (one(Int16) << bitidx)
+    end
+    return bitset
 end
