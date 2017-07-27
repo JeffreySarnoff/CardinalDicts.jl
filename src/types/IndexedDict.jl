c90a527a5bc3121945c69c525192178923fb8e4b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 03:18:32 -0400

    Create IndexedDict.jl

---
+++ src/types/IndexedDict.jl
@@ -0,0 +1,12 @@
+include("NBitSet.jl")
+
+struct IndexedDict{N, K<:Integer, V} <: Associative{K,V} where V where K where N
+    valued::NBitSet{N}
+    values::Vector{V}
+    
+    function IndexedDict{V}(N) where N<:Integer
+        valued = NBitSet{N}
+        values = Vector{V}(N)
+        return new{N,typeof(N),V}(valued, values)
+    end
+end
