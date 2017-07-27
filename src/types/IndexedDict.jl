3dccabc615e7ed50a401b2b2148b72e660a153cb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 14:33:29 -0400

    Update IndexedDict.jl

--- src/types/IndexedDict.jl
+++ src/types/IndexedDict.jl
@@ -8,20 +8,27 @@ struct IndexedDict{K,V} <: Associative{K,V}
     function IndexedDict{V}(n::K) where K<:Integer where V
         valued = NBitSet(n)
         values = Vector{V}(n)
+        if V<:Numeric
+           values[:] = zero(V)
+        elseif V<:String
+           values[:] = ""
+        else
+           values[:] = convert(V, 0)
+         end
         return new{K,V}(valued, values)
     end
 end
 
-@inline function Base.haskey(dict::IndexedDict{K,V}, key::K) where K<:Integer where V
+@inline function Base.haskey(dict::IndexedDict{K,V}, key::K) where K where V
    getindex(dict.valued, key%Int16)
 end
 
-function Base.getindex(dict::IndexedDict{K,V}, key::K) where K<:Integer where V
+function Base.getindex(dict::IndexedDict{K,V}, key::K) where K where V
     haskey(dict, key) && return dict[key]
     throw(ErrorException("Key (index) $(key) has not been given a value"))
 end
 
-function Base.setindex!(dict::IndexedDict{K,V}, value::V, key::K) where K<:Integer where V
+function Base.setindex!(dict::IndexedDict{K,V}, value::V, key::K) where K where V
     0 < key <= N || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(N)."))
     dict.values[key] = value
     return dict
