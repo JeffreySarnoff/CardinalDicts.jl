f4fa28646a2e8f4e0e579483a1d5120571568b62
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 04:42:02 -0400

    Update NBitSets.jl

--- src/types/NBitSets.jl
+++ src/types/NBitSets.jl
@@ -12,18 +12,19 @@ Base.length(bitset::NBitSet{N}) where N = N%Int16
 
 const One16 = one(Int16)
 
+@inline bitdex(index::Int16)::Int16 = fldmod(index, 16%Int16)
 @inline setbit(bits::Int16, index::Int16)::Int16 = bits | (One16 << (index-One16))
 @inline getbit(bits::Int16, index::Int16)::Int16 = (bits & (One16 << (index-One16))) !== zero(Int16)
 
 function Base.getindex(bitset::NBitSet{N}, index::I) where I where N
     0 < index <= N || throw(ErrorException("index $(index) is outside of the defined domain (1:$(N))")) 
-    offset, bitidx = fldmod(index%Int16, 16%Int16)
+    offset, bitidx = bitdex(index%Int16)
     return One16 === getbit(bitset.value[offset+One16], bitidx)
  end
 
 function Base.setindex!(bitset::NBitSet{N}, value::Bool, index::I) where I where N
     0 < index <= N || throw(ErrorException("index $(index) is outside of the defined domain (1:$(N))")) 
-    offset, bitidx = fldmod(index%Int16, 16%Int16)
+    offset, bitidx = bitdex(index%Int16)
     if value
         bitset.value[offset+One16] = setbit(bitset.value[offset+One16], bitidx)
     end
@@ -34,13 +35,13 @@ end
 
 function fast_setindex!(bitset::NBitSet{N}, index::I) where I where N
     0 < index <= N || throw(ErrorException("index $(index) is outside of the defined domain (1:$(N))")) 
-    offset, bitidx = fldmod(index%Int16, 16%Int16)
+    offset, bitidx = bitdex(index%Int16)
     bitset.value[offset+One16] = setbit(bitset.value[offset+One16], bitidx)
     return bitset
 end
 
 function unsafe_fast_setindex!(bitset::NBitSet{N}, index::I) where I where N
-    offset, bitidx = fldmod(index%Int16, 16%Int16)
+    offset, bitidx = bitdex(index%Int16)
     bitset.value[offset+One16] = setbit(bitset.value[offset+One16], bitidx)
     return bitset
 end
