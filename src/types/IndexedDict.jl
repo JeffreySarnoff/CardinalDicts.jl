d0c0510882a58e45fdd62f2b09562b8e86d0a556
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 04:54:58 -0400

    Update IndexedDict.jl

--- src/types/IndexedDict.jl
+++ src/types/IndexedDict.jl
@@ -1,6 +1,7 @@
 include("NBitSets.jl")
+using .NBitSets
 
-struct IndexedDict{N, K<:Integer, V} <: Associative{K,V} where V where K where N
+struct IndexedDict{N, K, V} <: Associative{K,V} where V where K where N
     valued::NBitSet{N}
     values::Vector{V}
     
@@ -10,3 +11,20 @@ struct IndexedDict{N, K<:Integer, V} <: Associative{K,V} where V where K where N
         return new{N,typeof(N),V}(valued, values)
     end
 end
+
+@inline function Base.haskey(dict::IndexedDict{N,,K,V}, key::K) where V where K<:Integer where N
+   getindex(dict.valued, key)
+end
+
+function Base.getindex(dict::IndexedDict{N,,K,V}, key::K) where V where K<:Integer where N
+    haskey(dict, key) && return dict[key]
+    throw(ErrorException("Key (index) $(key) has not been given a value"))
+end
+
+function Base.setindex!(dict::IndexedDict{N,,K,V}, value::V, key::K) where V where K<:Integer where N
+    0 < key <= N || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(N)."))
+    dict.values[key] = value
+    return dict
+end
+
+        
