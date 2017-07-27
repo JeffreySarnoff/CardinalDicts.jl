61379ebfb3e9cc675376bc46471e0c250d8d6109
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 05:59:10 -0400

    Update IndexedDict.jl

--- src/types/IndexedDict.jl
+++ src/types/IndexedDict.jl
@@ -6,24 +6,26 @@ struct IndexedDict{N, K, V} <: Associative{K,V}
     values::Vector{V}
     
     function IndexedDict{V1}(n::N1) where N1<:Integer where V1
-        valued = NBitSet{n}
-        values = Vector{V1}(n)
+        n16 = n%Int16
+        valued = NBitSet{n16}
+        values = Vector{V1}(n16)
         return new{N1,N1,V1}(valued, values)
     end
 end
 
 @inline function Base.haskey(dict::IndexedDict{N,K,V}, key::K) where V where K<:Integer where N
-   getindex(dict.valued, key)
+   getindex(dict.valued, key%Int16)
 end
 
 function Base.getindex(dict::IndexedDict{N,K,V}, key::K) where V where K<:Integer where N
-    haskey(dict, key) && return dict[key]
+    cle = key%Int16
+    haskey(dict, cle) && return dict[cle]
     throw(ErrorException("Key (index) $(key) has not been given a value"))
 end
 
 function Base.setindex!(dict::IndexedDict{N,K,V}, value::V, key::K) where V where K<:Integer where N
     0 < key <= N || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(N)."))
-    dict.values[key] = value
+    dict.values[key%Int16] = value
     return dict
 end
 
