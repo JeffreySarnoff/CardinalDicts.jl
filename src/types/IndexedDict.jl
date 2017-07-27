b0b94ccf14c80dae90fe1d4c0ce8fd1898118f53
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 05:29:53 -0400

    Update IndexedDict.jl

--- src/types/IndexedDict.jl
+++ src/types/IndexedDict.jl
@@ -5,18 +5,18 @@ struct IndexedDict{N, K, V} <: Associative{K,V} where V where K where N
     valued::NBitSet{N}
     values::Vector{V}
     
-    function IndexedDict{V}(N) where N<:Integer
-        valued = NBitSet{N}
-        values = Vector{V}(N)
-        return new{N,typeof(N),V}(valued, values)
+    function IndexedDict{V1}(N1) where N1<:Integer
+        valued = NBitSet{N1}
+        values = Vector{V1}(N1)
+        return new{N1,typeof(N1),V1}(valued, values)
     end
 end
 
-@inline function Base.haskey(dict::IndexedDict{N,,K,V}, key::K) where V where K<:Integer where N
+@inline function Base.haskey(dict::IndexedDict{N,K,V}, key::K) where V where K<:Integer where N
    getindex(dict.valued, key)
 end
 
-function Base.getindex(dict::IndexedDict{N,,K,V}, key::K) where V where K<:Integer where N
+function Base.getindex(dict::IndexedDict{N,K,V}, key::K) where V where K<:Integer where N
     haskey(dict, key) && return dict[key]
     throw(ErrorException("Key (index) $(key) has not been given a value"))
 end
