35f453a261d8be6170fd9a640fa0e72c186b1c96
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 03:57:57 -0400

    Update NBitSets.jl

--- src/types/NBitSets.jl
+++ src/types/NBitSets.jl
@@ -8,20 +8,19 @@ struct NBitSet{N}
     end
 end
 
-Base.length(bitset::NBitSet{N}) where N = N
-Base.typemax(bitset::NBitSet{N}) where N = fldmod(N, 16)
+Base.length(bitset::NBitSet{N}) where N = N%Int16
 
 function Base.getindex(bitset::NBitSet{N}, index::I) where I where N
     0 < index <= N || throw(ErrorException("index $(index) is outside of the defined domain (1:$(N))")) 
     offset, bitidx = fldmod(index%Int16, 16%Int16)
-    return one(Int16) === (bitset.value[offset+1] >> (15%Int16 - bitidx%Int16)) & one(Int16)
+    return one(Int16) === (bitset.value[offset+one(Int16)] >> (15%Int16 - bitidx%Int16)) & one(Int16)
 end
 
 function Base.setindex!(bitset::NBitSet{N}, value::Bool, index::I) where I where N
     0 < index <= N || throw(ErrorException("index $(index) is outside of the defined domain (1:$(N))")) 
     offset, bitidx = fldmod(index%Int16, 16%Int16)
     if value
-        bitset.value[offset+1] = bitset.value[offset+1] | (one(Int16) << bitidx)
+        bitset.value[offset+one(Int16)] = bitset.value[offset+one(Int16)] | (one(Int16) << bitidx)
     end
     return bitset
 end
