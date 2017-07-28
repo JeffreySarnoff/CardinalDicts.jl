f18b2146e1f0028869d0c88f32302d71929f2ad4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-27 21:07:14 -0400

    Update IndexedDict.jl

--- src/types/IndexedDict.jl
+++ src/types/IndexedDict.jl
@@ -1,26 +1,20 @@
 include("NBitSets.jl")
 using .NBitSets
 
+
 struct IndexedDict{K,V} <: Associative{K,V}
-    valued::NBitSet{K}
+    valued::BitArray{1}
     values::Vector{V}
     
     function IndexedDict{V}(n::K) where K<:Integer where V
-        valued = NBitSet(n)
+        valued = falses(n)
         values = Vector{V}(n)
-        if V<:Number
-           values[:] = zero(V)
-        elseif V<:String
-           values[:] = ""
-        else
-           values[:] = convert(V, 0)
-         end
         return new{K,V}(valued, values)
     end
 end
 
 @inline function Base.haskey(dict::IndexedDict{K,V}, key::K) where K where V
-   getindex(dict.valued, key)
+   return getindex(dict.valued, key)
 end
 
 function Base.getindex(dict::IndexedDict{K,V}, key::K) where K where V
@@ -32,12 +26,9 @@ function Base.get(dict::IndexedDict{K,V}, key::K, default::V) where K where V
     return haskey(dict, key) ? dict[key] : default
 end
 
-function Base.get(dict::IndexedDict{K,V}, key::K, default::S) where K where V where S
-    return haskey(dict, key) ? dict[key] : convert(V,default)
-end
-
 function Base.setindex!(dict::IndexedDict{K,V}, value::V, key::K) where K where V
     0 < key <= length(dict) || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(length(dict))."))
+    dict.valued[key] = true
     dict.values[key] = value
     return dict
 end
