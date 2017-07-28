d633b5a63eebbd78858e70d971d8d26e4287b0b2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-28 07:14:46 -0400

    Update IndexedDicts.jl

--- src/IndexedDicts.jl
+++ src/IndexedDicts.jl
@@ -2,6 +2,23 @@ module IndexedDicts
 
 export IndexedDict
 
+function bitsof(::Type{T}) = sizeof(T) * 8
+@inline function bitsof(::Type{T})::T where T<:Integer
+    return sizeof(T)%T << (one(T)+one(T)+one(T))
+end
+  
+function bits_required(posint::T)::T where T<:Integer
+    posint > 0 || throw(ErrorException("posint ($(posint)) must be > 0"))
+    bitsize = bitsof(T)
+    return bitsize - leading_zeros(posint)%T
+end
+
+const TypeForIndexing = [Int8, Int16, Int32, Int32, Int64, Int64, Int64, Int64, Int128 ]
+function signedtype_required(posint::T) where T<:Integer
+   type_index = nextpow2(bits_required(posint) + one(T)) >>> 3%T
+   return TypeForIndexing[ min(9%T, type_index) ]
+end
+  
 include("types/IndexedDict.jl")
 include("dict/workalike.jl")
 
