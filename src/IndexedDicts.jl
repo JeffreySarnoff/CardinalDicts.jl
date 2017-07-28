bc614de9d321afc719eed135fef0b8a040abfe0b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-28 07:40:44 -0400

    Update IndexedDicts.jl

--- src/IndexedDicts.jl
+++ src/IndexedDicts.jl
@@ -1,12 +1,19 @@
 module IndexedDicts
 
-export IndexedDict
+export IndexedDictonary
+
+#=
+    Julia is a bit even more snappy when indicies, bitshifts and other register-centric ops
+        are performed with Int16 or Int32 values.  Int64 values are very fast, too.
+     The functions I use to optimize the type of the indexing value are needed
+        when they are being leaned upon to provide time-minimized access.
+=#
 
 bitsof(::Type{T}) where T = sizeof(T) * 8
 @inline function bitsof(::Type{T})::T where T<:Integer
     return sizeof(T)%T << (one(T)+one(T)+one(T))
 end
-  
+ 
 function bits_required(posint::T)::T where T<:Integer
     posint > 0 || throw(ErrorException("posint ($(posint)) must be > 0"))
     bitsize = bitsof(T)
@@ -14,12 +21,13 @@ function bits_required(posint::T)::T where T<:Integer
 end
 
 const TypeForIndexing = [Int8, Int16, Int32, Int32, Int64, Int64, Int64, Int64, Int128 ]
-function signedtype_required(posint::T) where T<:Integer
+
+function type_for_indexing(posint::T) where T<:Integer
    type_index = nextpow2(bits_required(posint) + one(T)) >>> 3%T
    return TypeForIndexing[ min(9%T, type_index) ]
 end
   
-include("types/IndexedDict.jl")
-include("dict/workalike.jl")
+include("IndexedDict.jl")
+include("dict_api.jl")
 
 end # module
