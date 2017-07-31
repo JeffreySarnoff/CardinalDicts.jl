fc148ded37140f80205b31a1be68cbb1a94f5127
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-31 17:14:23 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,26 @@
-using IndexedDict
+using CardinalDicts
 using Base.Test
 
-@test 1 == 1
+#=
+  create an CardinalDict with indices 1:20 that holds Int64 values
+  populate it
+  use it
+  unset an index
+  check it
+  change an indexed value
+=#
+
+FactorialDict = CardinalDict{Int64}(20);
+
+for i in 1:20
+    setindex!(FactorialDict, factorial(i), i)
+end
+        
+@test haskey(FactorialDict, 17) == true
+@test FactorialDict[17] == factorial(17)
+
+clearindex!(FactorialDict, 17)
+@test haskey(FactorialDict, 17) == false
+@test get(FactorialDict, 17, 0) == 0
+FactorialDict[17] = factorial(17)
+@test FactorialDict[17] == factorial(17)
