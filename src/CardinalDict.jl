f53717b46938e2d5c49fac85954fd53b3b75f825
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-31 18:16:19 -0400

    Update CardinalDict.jl

--- src/CardinalDict.jl
+++ src/CardinalDict.jl
@@ -41,7 +41,7 @@ end
     setindex!(dict, value, key%K)
 
 function clearindex!(dict::CardinalDict{K,V}, key::K) where K where V
-    0 < key <= keymax(dict) || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(length(dict))."))
+    0 < key <= keymax(dict) || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(keymax(dict))."))
     @inbounds setindex!(dict.valued, false, key)
     return dict
 end
