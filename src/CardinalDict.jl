ca5fa91cad06cc42b6069c9b75df5fdf73d0b106
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-08-07 10:24:24 -0400

    Update CardinalDict.jl

--- src/CardinalDict.jl
+++ src/CardinalDict.jl
@@ -43,11 +43,3 @@ function Base.setindex!(dict::CardinalDict{K,V}, value::V, key::K) where {K,V}
 end
 @inline Base.setindex!(dict::CardinalDict{K,V}, value::V, key::J) where J where K where V =
     setindex!(dict, value, key%K)
-
-function clearindex!(dict::CardinalDict{K,V}, key::K) where K where V
-    0 < key <= keymax(dict) || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(keymax(dict))."))
-    @inbounds setindex!(dict.valued, false, key)
-    return dict
-end
-@inline clearindex!(dict::CardinalDict{K,V}, key::J) where J where K where V =
-    clearindex!(dict, key%K)
