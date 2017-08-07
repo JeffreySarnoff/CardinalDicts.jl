f8a02d6ad2590e275d88b96152b0bd84a136848d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-08-07 11:47:51 -0400

    Update dict_api_lite.jl

--- src/dict_api_lite.jl
+++ src/dict_api_lite.jl
@@ -60,14 +60,6 @@ end
 @inline Base.delete!(dict::CardinalDict{K,V}, key::J) where {J,K,V} =
     delete!(dict, key%K)
 
-function clearindex!(dict::CardinalDict{K,V}, key::K) where {K,V}
-    0 < key <= keymax(dict) || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(keymax(dict))."))
-    @inbounds setindex!(dict.valued, false, key)
-    return nothing
-end
-@inline clearindex!(dict::CardinalDict{K,V}, key::J) where {J,K,V} =
-    clearindex!(dict, key%K)
-
 function Base.empty!(dict::CardinalDict{K,V}) where {K,V}
     0 < key <= keymax(dict) || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(keymax(dict))."))
     for k in keys(dict)
