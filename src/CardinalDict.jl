8c292a4375face9c9031c9c76f0d029ecb53480a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-08-07 10:33:31 -0400

    Update CardinalDict.jl

--- src/CardinalDict.jl
+++ src/CardinalDict.jl
@@ -19,6 +19,7 @@ function CardinalDict(values::Vector{T}) where T
     return dict
 end
 
+@inline keymax(dict::CardinalDict{K,V}) where {K,V} = length(dict.valued)%K
 
 @inline function Base.haskey(dict::CardinalDict{K,V}, key::K) where {K,V}
    return getindex(dict.valued, key)
