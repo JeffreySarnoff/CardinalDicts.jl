ec680f8a1a9b714dee0c405951be8cce5ec02d8b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-08-08 00:25:47 -0400

    Update CardinalDict_api.jl

--- src/CardinalDict_api.jl
+++ src/CardinalDict_api.jl
@@ -7,7 +7,7 @@ isfull(dict::CardinalDict{K, V}) where {K,V} = all(dict.valued)
 function clearindex!(dict::CardinalDict{K,V}, key::K) where K where V
     0 < key <= keymax(dict) || throw(ErrorException("Key (index) $(key) is outside of the domain 1:$(keymax(dict))."))
     @inbounds setindex!(dict.valued, false, key)
-    return dict
+    return nothing
 end
 @inline clearindex!(dict::CardinalDict{K,V}, key::J) where J where K where V =
     clearindex!(dict, key%K)
