96e73e623a917a3dbbfc6e7f456ec655251f7fb6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-08-07 22:39:15 -0400

    Update dict_api_lite.jl

--- src/dict_api_lite.jl
+++ src/dict_api_lite.jl
@@ -113,19 +113,14 @@ function stringtoshow(dict::CardinalDict{K,V}) where {K,V}
         kv = [Pair(k,v) for (k,v) in zip(ks,vs)]
         str = string("CardinalDict(",kv,")")
     else
-        if ttyrows >= n
-            kv = [Pair(k,v) for (k,v) in zip(ks,vs)]
-            str = string("CardinalDict(",kv,")")
-        else
-            ttyrows = fld(ttyrows-2, 2)
-            kvfront = [Pair(k,v) for (k,v) in zip(ks[1:ttyrows], vs[1:ttyrows])]
-            kvback  = [Pair(k,v) for (k,v) in zip(ks[end-ttyrows:end], vs[end-ttyrows:end])]
-            sfront = string(kvfront)
-            sback  = string(kvback)
+        ttyrows = fld(ttyrows-2, 2)
+        kvfront = [Pair(k,v) for (k,v) in zip(ks[1:ttyrows], vs[1:ttyrows])]
+        kvback  = [Pair(k,v) for (k,v) in zip(ks[end-ttyrows:end], vs[end-ttyrows:end])]
+        sfront = string(kvfront)
+        sback  = string(kvback)
         str = string(sfront[findfirst(sfront,'}')+1:end-1],", ...\n  ", sback[findfirst(sback,'}')+2:end])
-            str = string("CardinalDict(", str, ")")
-            str = join(split(str,", "),",\n  ")
-        end
+        str = string("CardinalDict(", str, ")")
+        str = join(split(str,", "),",\n  ")
     end
     return str
 end
